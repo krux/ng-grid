37318560fe1f3426a65c56cb97f777b79ee4f7a8
siarheiNaralenkau <naralenkov2010@gmail.com>
2015-06-03 11:59:16 +0300

    Japanize localization was added

---
+++ src/i18n/ja.js
@@ -0,0 +1,14 @@
+window.ngGrid.i18n['ja'] = {
+    ngAggregateLabel: 'アイテム',
+    ngGroupPanelDescription: 'ここで列ヘッダをドラッグして、その列によってグループにドロップします。',
+    ngSearchPlaceHolder: '検索...',
+    ngMenuText: '列の選択：',
+    ngShowingItemsLabel: 'アイテムを表示：',
+    ngTotalItemsLabel: 'アイテム数：',
+    ngSelectedItemsLabel: '選択したアイテム：',
+    ngPageSizeLabel: 'ページサイズ：',
+    ngPagerFirstTitle: '先頭ページ',
+    ngPagerNextTitle: '次のページへ',
+    ngPagerPrevTitle: '前ページ',
+    ngPagerLastTitle: '最終ページ'
+}
